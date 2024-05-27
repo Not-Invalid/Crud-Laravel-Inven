284a38ae3155b8b1fe15695faa9f8e13863ee919
RaffDev31 <raffli4000@hotmail.com>
2024-05-27 11:12:51 +0700

    feat:add function for cetak

--- app/Http/Controllers/InventarisController.php
+++ app/Http/Controllers/InventarisController.php
@@ -1,27 +1,56 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use App\Models\Inventaris;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
+use App\Models\Barang;
+use App\Models\JenisBarang;
 use Illuminate\Support\Facades\DB;
 
 class InventarisController extends Controller
 {
     public function index()
     {
-        $inventaris = DB::table('inventaris')
-            ->join('barangs', 'inventaris.kode_barang', '=', 'barangs.kode_barang')
-            ->leftJoin('pemakaians', 'inventaris.id', '=', 'pemakaians.id_pemakaian')
+        $inventaris = DB::table('pemakaians')
+            ->join('barangs', 'pemakaians.kode_barang', '=', 'barangs.kode_barang')
+            ->join('jenis_barangs', 'barangs.jenis_barang', '=', 'jenis_barangs.jenis_barang')
+            ->join('ruangans', 'pemakaians.id_ruangan', '=', 'ruangans.id_ruangan')
             ->select(
-                'inventaris.kode_barang',
-                'barangs.nama_barang',
+                'pemakaians.id_pemakaian as id',
+                'pemakaians.kode_barang',
+                'pemakaians.nama_barang',
+                'jenis_barangs.jenis_barang as jenis_barang',
                 'barangs.created_at as tanggal_pembelian',
                 'pemakaians.tanggal_pakai as tanggal_pemakaian',
+                'ruangans.nama_ruangan as ruangan',
                 'pemakaians.keterangan'
             )
             ->get();
 
-        return view('inventaris.index', compact('inventaris'));
+        return view('inventaris.index', ['inventaris' => $inventaris]);
     }
 
-};    
+    public function cetak()
+    {
+        $data = DB::table('pemakaians')
+            ->join('barangs', 'pemakaians.kode_barang', '=', 'barangs.kode_barang')
+            ->join('jenis_barangs', 'barangs.jenis_barang', '=', 'jenis_barangs.jenis_barang')
+            ->join('ruangans', 'pemakaians.id_ruangan', '=', 'ruangans.id_ruangan')
+            ->select(
+                'pemakaians.id_pemakaian as id',
+                'pemakaians.kode_barang',
+                'pemakaians.nama_barang',
+                'jenis_barangs.jenis_barang as jenis_barang',
+                'barangs.created_at as tanggal_pembelian',
+                'pemakaians.tanggal_pakai as tanggal_pemakaian',
+                'ruangans.nama_ruangan as ruangan',
+                'pemakaians.keterangan'
+            )
+            ->orderBy('pemakaians.id_pemakaian', 'DESC')
+            ->get();
+
+        $pdf = Pdf::loadView('inventaris.cetak', ['data' => $data])->setPaper('a4', 'portrait');
+        return $pdf->stream('laporan_inventaris.pdf');
+    }
+}
