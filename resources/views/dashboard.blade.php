ee4bcf2c78ff37450df471a2e3271153d35bdd46
RaffDev31 <raffli4000@hotmail.com>
2024-05-28 09:17:46 +0700

    feat: Added alert login success

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
   
-@section('title', 'Dashboard - CRUD Barang')
+@section('title')
   
 @section('contents')
   <div class="row">
-    Dashboard
+    <div class="col-md-12">
+      <div class="alert alert-success" role="alert">
+        You are logged in
+      </div>
+    </div>
+    <div class="col-md-12">
+      Dashboard
+    </div>
   </div>
 @endsection
